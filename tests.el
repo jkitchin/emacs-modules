af3aa264c3ebce170fdb6e991c7808c32b4a2d9b
John Kitchin <jkitchin@andrew.cmu.edu>
2017-07-10 18:35:09 -0400

    add constants test

--- tests.el
+++ tests.el
@@ -2,9 +2,15 @@
 
 (defun test-linalg () 
   (require 'gsl-linalg)
-  (princ (gsl-linalg-LU-solve
+  (print (gsl-linalg-LU-solve
 	  [[0.18 0.60 0.57 0.96]
 	   [0.41 0.24 0.99 0.58]
 	   [0.14 0.30 0.97 0.66]
 	   [0.51 0.13 0.19 0.85]]
 	  [1.0 2.0 3.0 4.0])))
+
+
+(defun test-constants ()
+  (require 'gsl-constants)
+  (print GSL-CONST-MKSA-PLANCKS-CONSTANT-H)
+  (print (describe-variable 'GSL-CONST-MKSA-PLANCKS-CONSTANT-H)))
